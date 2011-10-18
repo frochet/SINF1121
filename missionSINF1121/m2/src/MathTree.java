c2f595481815a7697a69ed9cadb50311ad713bb2
Florentin Rochet <oversnals@hotmail.com>
2011-10-18 11:21:04 +0200

    MathTree

--- missionSINF1121/m2/src/MathTree.java
+++ missionSINF1121/m2/src/MathTree.java
@@ -1,4 +1,24 @@
 
-public class MathTree {
+public class MathTree<E> extends LinkedRBinaryTree implements FormalExpressionTree {
+
+	public MathTree(Position<E> root, Position<E> parent, RBinaryTree<E> leftTree,
+			RBinaryTree<E> rightTree, E element) {
+		super(root, parent, leftTree, rightTree);
+		// TODO Auto-generated constructor stub
+		setElement(element);
+	}
+
+	@Override
+	public FormalExpressionTree derive() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public String toString(){
+		return null;
+	}
+	
+	// Besoin de methodes d'ajout ?
 
 }
