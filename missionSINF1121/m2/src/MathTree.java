9c34a6020138bb667599da5ade3f9dc1acfd9400
Claude Carpentier <claude.carpentier@student.uclouvain.be>
2011-10-19 17:46:42 +0200

    toString et derive presque ok

--- missionSINF1121/m2/src/MathTree.java
+++ missionSINF1121/m2/src/MathTree.java
@@ -10,128 +10,197 @@ public class MathTree extends LinkedRBinaryTree<String> implements FormalExpress
 	public MathTree(String element) {
 		super (null, null, element);
 	}
+	
+	@Override
+	public String toString(){	
+		if (leftTree==null & rightTree==null)
+		{
+			return element;
+		}
+	
+		else if (leftTree==null)
+		{
+			return element+" ("+rightTree.toString()+")";
+		}
+		else if (rightTree==null)
+		{
+			return leftTree.toString();
+		}
+		
+		else
+		{
+			return "("+leftTree.toString()+") "+element+" ("+rightTree.toString()+")";
+		}
+	}
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public FormalExpressionTree derive() {
-		
-		
-		/*	public static void main(String[] args)
-	{
-		Integer i = 17;
-		String str = "19";
-		System.out.println( i.getClass() );
-		System.out.println( str.getClass() );
-
 	
-	}
-	
-	Résultat : 
-	
-	class java.lang.Integer
-	class java.lang.String
-
-Ca me semblait audacieux, je vais corriger ça ^^
-*/
-		if(this.element().getClass()==java.lang.Number)
+		if (isLeaf())
 		{
-			return null;
-		}
+			if (element()=="x")
+			{
+				return new MathTree("1");
+			}
 			
-		if(element()=="+")
-		{			
-			return new MathTree(
-						(RBinaryTree<String>)((MathTree)leftTree).derive(),
-						(RBinaryTree<String>)((MathTree)(rightTree)).derive(),
-						element);
+			else return null;
 		}
-	
-		if (element()=="-")
+		else
 		{
-			return new MathTree(
-						(RBinaryTree<String>)((MathTree)(leftTree)).derive(),
-						(RBinaryTree<String>)((MathTree)(rightTree)).derive(),
-						element);
-		}
+			if(element()=="+")
+			{			
+				if (((MathTree)leftTree).derive()==null)
+				{
+					return ((MathTree)(rightTree)).derive();
+				}
+				else if (((MathTree)rightTree).derive()==null)
+				{
+					return ((MathTree)(leftTree)).derive();
+				}
+				else
+				{
+					return new MathTree(
+							(RBinaryTree<String>)((MathTree)leftTree).derive(),
+							(RBinaryTree<String>)((MathTree)rightTree).derive(),
+							"+");
+				}
+			}
 		
-		if (this.element()=="*")
-		{	
-			return new MathTree(
-						new MathTree(
-								leftTree,
-								(RBinaryTree<String>)((MathTree)(rightTree)).derive(),
-								element),
-						new MathTree(
-								(RBinaryTree<String>)((MathTree)(leftTree)).derive(),
-								rightTree,
-								element),
-						"+");
-		}
-		
-		if (this.element()=="/")
-		{
-			return new MathTree(
-						new MathTree(
-								new MathTree(
-										rightTree,
-										(RBinaryTree<String>)((MathTree)(leftTree)).derive(),
-										"*"),
-								new MathTree(
-										(RBinaryTree<String>)((MathTree)(rightTree)).derive(),
-										leftTree,
-										"*"),
-								"-"),
-						new MathTree(
-										rightTree,
-										new MathTree("2"),
-										"exp"),
-						"/");
-		}
-		
-		
-		if (this.element()=="sin")
-		{
-			return new MathTree(
-					(RBinaryTree<String>)((MathTree)(leftTree)).derive(),
+			else if (element()=="-")
+			{
+				if (((MathTree)rightTree).derive()==null)
+				{
+					return ((MathTree)(leftTree)).derive();
+				}
+				else
+				{
+					return new MathTree(
+							(RBinaryTree<String>)((MathTree)leftTree).derive(),
+							(RBinaryTree<String>)((MathTree)rightTree).derive(),
+							"-");
+				}
+			}
+			
+			else if (this.element()=="*")
+			{	
+				if (((MathTree)leftTree).derive()==null)
+				{
+					return new MathTree(
+							leftTree,
+							(RBinaryTree<String>)((MathTree)(rightTree)).derive(),
+							"*");
+				}
+				else if (((MathTree)rightTree).derive()==null)
+				{
 					new MathTree(
+							(RBinaryTree<String>)((MathTree)(leftTree)).derive(),
+							rightTree,
+							"*");
+				}
+				else if (((MathTree)rightTree).derive()==null & ((MathTree)leftTree).derive()==null)
+				{
+					return null;
+				}
+				else
+				{
+					return new MathTree(
+							new MathTree(
+									leftTree,
+									(RBinaryTree<String>)((MathTree)(rightTree)).derive(),
+									"*"),
+							new MathTree(
+									(RBinaryTree<String>)((MathTree)(leftTree)).derive(),
+									rightTree,
+									"*"),
+							"+");
+				}
+			}
+			
+			else if (this.element()=="/") // A modifier
+			{
+				return new MathTree(
+							new MathTree(
+									new MathTree(
+											rightTree,
+											(RBinaryTree<String>)((MathTree)(leftTree)).derive(),
+											"*"),
+									new MathTree(
+											(RBinaryTree<String>)((MathTree)(rightTree)).derive(),
+											leftTree,
+											"*"),
+									"-"),
+							new MathTree(
+											rightTree,
+											new MathTree("2"),
+											"^"),
+							"/");
+			}
+			
+			
+			else if (this.element()=="sin")
+			{
+				if (((MathTree)(leftTree)).derive()==null)
+				{
+					return new MathTree(
 							leftTree,
 							null,
-							"cos"),
-					"*");								
-		}
-		
-		if (this.element()=="cos")
-		{
-			return new MathTree(
-					null,
-					new MathTree(				
+							"cos");
+				}
+				else
+				{
+					return new MathTree(
 							(RBinaryTree<String>)((MathTree)(leftTree)).derive(),
 							new MathTree(
 									leftTree,
 									null,
 									"cos"),
-							"*"),
-					"-");								
+							"*");
+				}								
+			}
+			
+			else if (this.element()=="cos")
+			{
+				if (((MathTree)(leftTree)).derive()==null)
+				{
+					return new MathTree(
+							leftTree,
+							null,
+							"sin");
+				}
+				else
+				{
+				return new MathTree(
+						null,
+						new MathTree(				
+								(RBinaryTree<String>)((MathTree)(leftTree)).derive(),
+								new MathTree(
+										leftTree,
+										null,
+										"sin"),
+								"*"),
+						"-");
+				}
+			}
+			
+			else if (this.element()=="^") //A modifier
+			{
+				return new MathTree(
+						rightTree,
+						new MathTree(
+								new MathTree(
+										leftTree,
+										rightTree, //-1
+										"^"),
+								(RBinaryTree<String>)((MathTree)(rightTree)).derive(),
+								"*"),
+						"*");								
+			}
 		}
+		return null;
 		
-		if (this.element()=="^")
-		{
-			return new MathTree(
-					rightTree,
-					new MathTree(
-							new MathTree(
-									leftTree,
-									rightTree, //-1
-									"^"),
-							(RBinaryTree<String>)((MathTree)(rightTree)).derive(),
-							"*"),
-					"*");								
-		}
-	}
-	
-	@Override
-	public String toString(){	
-		return "("+leftTree.toString()+") "+element+" ("+rightTree.toString()+")";
+
+		
 	}
 	
 }
