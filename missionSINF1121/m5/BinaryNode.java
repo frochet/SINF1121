8415c21e4d7a5a3dc072904f2a014537ce21b39b
Florentin Rochet <oversnals@hotmail.com>
2011-12-06 13:36:49 +0100

    first commit

---
+++ missionSINF1121/m5/BinaryNode.java
@@ -0,0 +1,11 @@
+
+public class BinaryNode<K> {
+	private K elem;
+	private BinaryNode<K> parent, rigth,left;
+	public BinaryNode(K elem, BinaryNode<K> parent, BinaryNode<K> left, BinaryNode<K> righ){
+		this.elem = elem;
+		this.parent = parent;
+		this.left = left;
+		this.rigth = rigth;
+	}
+}
---
+++ missionSINF1121/m5/BinaryTree.java
@@ -0,0 +1,13 @@
+
+public class BinaryTree<K> {
+	
+	private BinaryNode<K> root;
+	
+	public BinaryTree(){
+		
+	}
+	public void setRoot(BinaryNode<K> root){
+		this.root = root;
+	}
+	
+}
