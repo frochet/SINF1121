9f628d78ac383ec1ecb7a61c563be0bd48728316
Valery Gerard <sakuraforever@gmail.com>
2011-12-06 20:11:13 +0100

    création de la classe Decompress
    elle n'existait pas

---
+++ missionSINF1121/m5/src/Decompress.java
@@ -0,0 +1,23 @@
+/*
+ * @author : Gérard Valéry
+ */
+public class Decompress {
+	private String fileInput,fileDecompressed;
+	
+	public Decompress(String fileInput , String fileDecompressed){
+		this.fileInput = fileInput;
+		this.fileDecompressed = fileDecompressed;
+		
+	}
+	// Decompress 'fileInput' file to 'fileDecompressed'
+	public void decompress(){
+		try {
+			//TODO
+			;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
