0584cf12554105aafb90a11d4656bc71ee27737b
Abdelali Ajrhourh <abdelali.ajrhourh@student.uclouvain.be>
2011-12-07 20:16:16 +0100

    Modif Abdel

---
+++ missionSINF1121/m5/InOutException.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *Classe InOutException
+ * @author Abdel
+ */
+public class InOutException extends Exception {
+
+    /**
+     * Creates a new instance of <code>InOutException</code> without detail message.
+     */
+    public InOutException() {
+    }
+
+
+    /**
+     * Constructs an instance of <code>InOutException</code> with the specified detail message.
+     * @param msg the detail message.
+     */
+    public InOutException(String msg) {
+        super(msg);
+    }
+}
